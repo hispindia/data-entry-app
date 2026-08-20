070d2ce754e8f6dcc2163ffeaab99a690e89ffc8
MohdNawazHISP <mohd.nawaz@hispindia.org>
2026-08-20 12:42:33 +0530

    Update: Submit Btn for Ed

--- assets/js/api/BaseApi.js
+++ assets/js/api/BaseApi.js
@@ -1,5 +1,5 @@
 const baseUrl = '../..';
-// const baseUrl = 'https://links.hispindia.org/ippf_co/api';
+// const baseUrl = 'https://links.hispindia.org/ippf_bpr/api';
 const ApiToken = '';
 
 const BaseApi = async ({ url, method, payload, mode }) => {
